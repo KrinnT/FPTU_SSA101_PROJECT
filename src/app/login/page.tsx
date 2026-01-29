7b972601d2e57b1c8f6c31f8c64c27d63407c726
khtwnq <khanhtuong2302@gmail.com>
2026-01-30 00:35:07 +0700

    fix: login forgot password link

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -76,7 +76,7 @@ function LoginForm() {
                         <div className="space-y-2">
                             <div className="flex items-center justify-between">
                                 <Label htmlFor="password">Password</Label>
-                                <Link href="#" className="text-xs text-primary underline-offset-4 hover:underline">
+                                <Link href="/forgot-password" className="text-xs text-primary underline-offset-4 hover:underline">
                                     Forgot password?
                                 </Link>
                             </div>
