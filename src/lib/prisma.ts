dc4fa7670af961c8dba0e65741830932141496ee
khtwnq <khanhtuong2302@gmail.com>
2026-03-06 12:14:33 +0700

    Hardcode Neon DB URL to force new database on Vercel

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -6,6 +6,11 @@ export const prisma =
     globalForPrisma.prisma ||
     new PrismaClient({
         log: ["query"],
+        datasources: {
+            db: {
+                url: "postgresql://neondb_owner:npg_OQiSBHLd1s7a@ep-frosty-thunder-a1z54541-pooler.ap-southeast-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require",
+            },
+        },
     });
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
